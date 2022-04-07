77abcefb4641a117304b09a7c468a58f4c817349
Mwesiga Benjamin <37869617+mwesigab@users.noreply.github.com>
2022-04-07 09:35:42 +0300

    New Changes On Form Submission

--- js/scripts.js
+++ js/scripts.js
@@ -74,21 +74,3 @@
 	});
 
 })(jQuery);
-
-document
-  .querySelector("form")
-  .addEventListener("submit", handleSubmit);
-
-const handleSubmit = (e) => {
-    alert("Hello")
-  /* e.preventDefault();
-  let myForm = document.getElementById("pizzaOrder");
-  let formData = new FormData(myForm);
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => console.log("Form successfully submitted"))
-    .catch((error) => alert(error)); */
-};
